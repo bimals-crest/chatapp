a8cf91fa80c70c01cf12705b04c2516576f92a96
bimals-crest <bimal.s@crestinfosystems.com>
2024-09-16 15:16:52 +0530

    Updates

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='chatapp',  # Package name
+    version='0.1.0',  # Initial version
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',  # License type
+    description='A Django app for chat functionality',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/chatapp',  # Replace with your repo URL
+    author='Your Name',
+    author_email='youremail@example.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 3.0',  # Adjust based on Django version
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'Django>=3.0',  # Add your dependencies here
+    ],
+    python_requires='>=3.6',
+)
